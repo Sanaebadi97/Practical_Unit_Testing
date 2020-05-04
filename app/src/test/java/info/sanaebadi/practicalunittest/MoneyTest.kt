50d126c3c49d3cc21e175f89c96cde91ebd50277
Sana Ebadi <sanaebadi97@gmail.com>
2020-05-04 13:32:51 -0400

    added EUR in Money

--- app/src/test/java/info/sanaebadi/practicalunittest/MoneyTest.kt
+++ app/src/test/java/info/sanaebadi/practicalunittest/MoneyTest.kt
@@ -4,15 +4,20 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class MoneyTest {
+
+    lateinit var money: Money
+
     @Test
     internal fun constructorShouldSetAmountAnCurrency() {
-        val money = Money(15, "USD")
+        money = Money(10, "USD")
         assertEquals(10, money.amount)
 
         println("AMOUNT ${money.amount}")
 
         assertEquals("USD", money.currency)
         println("CURRENCY ${money.currency}")
+
+        money = Money(20, "EUR")
 
     }
 }
