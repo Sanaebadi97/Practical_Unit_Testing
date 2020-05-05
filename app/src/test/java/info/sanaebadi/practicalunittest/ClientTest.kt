400fc63e1d54a4b28c7c0db144117c488a1f9e53
Sana Ebadi <sanaebadi97@gmail.com>
2020-05-05 06:36:19 -0400

    added Client class

--- app/src/test/java/info/sanaebadi/practicalunittest/ClientTest.kt
+++ app/src/test/java/info/sanaebadi/practicalunittest/ClientTest.kt
@@ -1,6 +1,13 @@
 package info.sanaebadi.practicalunittest
 
+import org.junit.jupiter.api.Test
+
 class ClientTest {
     val addressA = Address("Street A")
     val addressB = Address("Street Bl")
+
+    @Test
+    fun afterCreationShouldHaveNoAddress() {
+
+    }
 }
