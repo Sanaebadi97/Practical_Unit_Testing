984b4bd2280c5747cde6ec8f90729e461c65e461
Sana Ebadi <sanaebadi97@gmail.com>
2020-05-05 05:25:44 -0400

    added some condition in init fun in Money class

--- app/src/main/java/info/sanaebadi/practicalunittest/Money.kt
+++ app/src/main/java/info/sanaebadi/practicalunittest/Money.kt
@@ -1,9 +1,17 @@
 package info.sanaebadi.practicalunittest
 
-data class Money(
+data class Money constructor(
     val amount: Int,
     val currency: String
 ) {
+
+
+
+    init {
+        require(amount >= 0) { "Illegal amount :[$amount]" }
+        require((currency == null || currency.isEmpty())) { "Illegal currency : [$currency]" }
+
+    }
 
     override fun equals(obj: Any?): Boolean {
         if (obj is Money) {
