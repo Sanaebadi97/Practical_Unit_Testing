cafef3a7b5f69dcab04f9fc1cf55b9b4b4e7567f
Sana Ebadi <sanaebadi97@gmail.com>
2020-05-11 10:28:00 -0400

    remove the method

--- app/src/test/java/info/sanaebadi/practicalunittest/FootballTeamTest.kt
+++ app/src/test/java/info/sanaebadi/practicalunittest/FootballTeamTest.kt
@@ -4,26 +4,27 @@ import info.sanaebadi.ch4.FootballTeam
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
-import org.junit.runners.Parameterized
 import kotlin.test.assertEquals
 
 class FootballTeamTest {
 
     companion object {
         const val THERE_GAMES_WON = 3
+
+
+//        fun nbOfGamesWon(): Array<Int> {
+//            return arrayOf(0, 2, 2)
+//        }
     }
 
-    private fun nbOfGamesWon(): Array<Int> {
-        return `$`(0, 1, 2)
-    }
 
     @Test
     @ParameterizedTest
     @MethodSource("nbOfGamesWon")
     fun constructorShouldSetGamesWon() {
-        val team = FootballTeam(nbOfGamesWon())
+        val team = FootballTeam(THERE_GAMES_WON)
         assertEquals(
-            nbOfGamesWon(),
+           THERE_GAMES_WON,
             team.gamesWon,
             "3 games passed to contractor , but ${team.gamesWon} , were returned $THERE_GAMES_WON , ${team.gamesWon}"
         )
