b5040012201969bf93f3ae5f94766f9f9b0ed2e8
Sana Ebadi <sanaebadi97@gmail.com>
2020-05-11 10:09:58 -0400

    added MethodSource

--- app/src/main/java/info/sanaebadi/ch4/FootballTeam.kt
+++ app/src/main/java/info/sanaebadi/ch4/FootballTeam.kt
@@ -1,5 +1,19 @@
 package info.sanaebadi.ch4
 
-data class FootballTeam(val gamesWon: Int=0) {
+data class FootballTeam(val gamesWon: Array<Int>) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as FootballTeam
+
+        if (!gamesWon.contentEquals(other.gamesWon)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return gamesWon.contentHashCode()
+    }
 
 }
--- app/src/test/java/info/sanaebadi/practicalunittest/FootballTeamTest.kt
+++ app/src/test/java/info/sanaebadi/practicalunittest/FootballTeamTest.kt
@@ -2,6 +2,9 @@ package info.sanaebadi.practicalunittest
 
 import info.sanaebadi.ch4.FootballTeam
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import org.junit.runners.Parameterized
 import kotlin.test.assertEquals
 
 class FootballTeamTest {
@@ -10,15 +13,17 @@ class FootballTeamTest {
         const val THERE_GAMES_WON = 3
     }
 
-    fun nbOfGamesWon(): Array<Any> {
+    private fun nbOfGamesWon(): Array<Int> {
         return `$`(0, 1, 2)
     }
 
     @Test
+    @ParameterizedTest
+    @MethodSource("nbOfGamesWon")
     fun constructorShouldSetGamesWon() {
-        val team = FootballTeam(THERE_GAMES_WON)
+        val team = FootballTeam(nbOfGamesWon())
         assertEquals(
-            THERE_GAMES_WON,
+            nbOfGamesWon(),
             team.gamesWon,
             "3 games passed to contractor , but ${team.gamesWon} , were returned $THERE_GAMES_WON , ${team.gamesWon}"
         )
